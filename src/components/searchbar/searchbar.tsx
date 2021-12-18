82847b86a3c55af39f75fd8a58bf66b2cc5411fe
vadymshukr <vshugaev@digitexfutures.com>
2021-12-18 16:37:16 +0200

    search added

--- src/components/searchbar/searchbar.tsx
+++ src/components/searchbar/searchbar.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Button } from '../button';
 import { Container, Input, SearchTitle } from './searchbar-styled';
 
-export function Searchbar () {
+interface Props {
+    handleSearch: (value: string) => void
+}
+
+export function Searchbar ({ handleSearch }: Props) {
+    const [inputValue, setInputValue] = useState('')
     return (
         <Container>
             <SearchTitle>Search:</SearchTitle>
-            <Input />
-            <Button type='main'>Search</Button>
+            <Input onBlur={(e) => {setInputValue(e.target.value)}} />
+            <Button type='main' onClick={() => {handleSearch(inputValue)}}>Search</Button>
         </Container>
     )
 }
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,29 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Button, CreateNote, Note, Searchbar } from '../../components';
-import { getNotesList } from '../../store/notes';
+import { getNotesList, notesType } from '../../store/notes';
 import { Container, NotesList, TopNavigation } from './home-styled';
 import Modal from 'react-modal';
 Modal.setAppElement('#root');
 
 export function Home () {
     const [modalIsOpen, setModalIsOpen] = useState(false)
+    const [visibleNotes, setVisibleNotes] = useState([] as notesType[]);
     const notes = useSelector(getNotesList)
+
+    useEffect(() => {
+        setVisibleNotes(notes)
+    }, [])
+
+    const handleSearch = (value: string) => {
+        const filteredNotes = notes.filter(note => note.title.includes(value) || note.content.includes(value))
+        setVisibleNotes(filteredNotes)
+    }
     return (
         <>
-        <Container>
-            <TopNavigation>
-                {/*<Searchbar />*/}
-                <Button onClick={() => {setModalIsOpen(true)}}>
-                    Create note
-                </Button>
-            </TopNavigation>
-            <NotesList>
-                {notes.map( item => (
-                    <Note content={item.content} id={item.id} title={item.title} key={item.id}/>
-                ))}
-            </NotesList>
-        </Container>
+            <Container>
+                <TopNavigation>
+                    <Searchbar handleSearch={handleSearch}/>
+                    <Button onClick={() => {setModalIsOpen(true)}}>
+                        Create note
+                    </Button>
+                </TopNavigation>
+                {visibleNotes.length > 0 ?
+                <NotesList>
+                    {visibleNotes.map( item => (
+                        <Note content={item.content} id={item.id} title={item.title} key={item.id}/>
+                    ))}
+                </NotesList>
+                    :
+                    <h1>Sorry, you don't have any notes or notes not includes your search query</h1>
+                }
+
+            </Container>
             <Modal
                 isOpen={modalIsOpen}
                 style={{content: {
